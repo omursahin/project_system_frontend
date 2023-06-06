ddf85cc8715b4fed2f4e4ef09191216f6ca9d5f8
zahidsgn <zahide.dusgun@gmail.com>
2023-06-06 18:38:50 +0300

    SemesterCourse tasarımı yapıldı

--- src/pages/SemesterCoursePage/SemesterCourseModal.js
+++ src/pages/SemesterCoursePage/SemesterCourseModal.js
@@ -1,40 +1,71 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
-import { PlusSquareDotted, PencilFill } from 'react-bootstrap-icons';
+import {PlusSquareDotted, PencilFill} from 'react-bootstrap-icons';
+import {FormControl} from "react-bootstrap";
+import {
+    useReportCreateMutation,
+    useReportUpdateMutation,
+} from "../../store/api/reports";
+import {useDispatch} from "react-redux";
+import {notificationActions} from "../../store/notification/notification-slice";
+import {useGetAllCoursesQuery} from "../../store/api/courses";
+import useGetAllSemesters from "../../store/api/semesters";
 
-function SemesterCourseModal({ isEdit = false, data = {} }) {
+
+function SemesterCourseModal({isEdit = false, SemesterCourseData = {}}) {
+    const [year, setYear] = useState(SemesterCourseData?.year);
+    const [term, setTerm] = useState(SemesterCourseData?.term?.id);
+    const [courses, setCourses] = useState(SemesterCourseData?.courses?.id);
+    const [max_group_size, setMax_group_size] = useState(SemesterCourseData?.max_group_size);
+
 
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    const dispatch = useDispatch();
+    const [create] = useReportCreateMutation();
+    const [update] = useReportUpdateMutation();
 
     const save = async () => {
         let response = null;
-        if (data?.id) {
-            // Güncelleme
-            alert("Güncelleme")
-
+        if (SemesterCourseData?.id) {
+            const payload = {
+                id: SemesterCourseData.id,
+                year: year,
+                term: term,
+                courses: courses,
+                max_group_size: max_group_size,
+            };
+            response = await update(payload);
         } else {
-            // Yeni veri ekleme
-            alert("Yeni veri ekleme")
+            const payload = {
+                year,
+                term,
+                courses,
+                max_group_size,
+            };
+            response = await create(payload);
         }
 
-        // if (response.error) {
-        //     dispatch(notificationActions.showMessage({
-        //         header: "Hata",
-        //         message: "Bir hata ile karşılaşıldı...",
-        //         variant: "danger"
-        //     }));
-        // } else {
-        //     dispatch(notificationActions.showMessage({
-        //         header: "Giriş",
-        //         message: "Başarı ile eklendi/güncellendi",
-        //         variant: "success"
-        //     }));
-        // }
+        if (response.error) {
+            dispatch(
+                notificationActions.showMessage({
+                    header: "Hata",
+                    message: "Bir hata ile karşılaşıldı...",
+                    variant: "danger"
+                })
+            );
+        } else {
+            dispatch(notificationActions.showMessage({
+                    header: "Giriş",
+                    message: "Başarı ile eklendi/güncellendi",
+                    variant: "success"
+                })
+            );
+        }
         setShow(false);
     };
 
@@ -44,33 +75,59 @@ function SemesterCourseModal({ isEdit = false, data = {} }) {
                 variant={isEdit ? "outline-primary" : "primary"}
                 onClick={handleShow}
             >
-                {isEdit ? <PencilFill size={15}></PencilFill> : <PlusSquareDotted size={20} />}
-                <span className="d-none d-md-block" >{isEdit ? "Düzenle" : "Ders Ekle"}</span>
+                {isEdit ? <PencilFill size={15}></PencilFill> : <PlusSquareDotted size={20}/>}
+                <span className="d-none d-md-block">{isEdit ? "Düzenle" : "Dönem Dersi Ekle"}</span>
             </Button>
 
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
-                    <Modal.Title>Dönem {isEdit ? "Güncelle" : "Ekle"}</Modal.Title>
+                    <Modal.Title>Dönem Dersi {isEdit ? "Güncelle" : "Ekle"}</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <Form>
-                        <Form.Group className="mb-3" controlId="term">
-                            <Form.Label>Dönem</Form.Label>
-                            <Form.Select aria-label="term" value={data?.term} onChange={(e) => {}}>
-                                <option value={0}>Güz</option>
-                                <option value={1}>Bahar</option>
-                                <option value={2}>Yaz</option>
-                            </Form.Select>
-                        </Form.Group>
                         <Form.Group
                             className="mb-3"
                             controlId="year"
                         >
                             <Form.Label>Yıl</Form.Label>
-                            <Form.Control
-                                onChange={(e) => {}}
+                            <FormControl
+                                onChange={(e) => {
+                                    setYear(e.target.value)
+                                }}
                                 type="number"
-                                value={data?.year || ''}
+                                value={year || ''}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="term">
+                            <Form.Label>Dönem</Form.Label>
+                            <Form.Select aria-label="term"
+                                         value={term || ''}
+                                         onChange={(e) => {
+                                             setTerm(e.target.value)
+                                         }}>
+                                <option value={0}>Güz</option>
+                                <option value={1}>Bahar</option>
+                                <option value={2}>Yaz</option>
+                            </Form.Select>
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="courses">
+                            <Form.Label>Ders</Form.Label>
+                            <Form.Control
+                                aria-label="courses"
+                                defaultValue={courses || ''}
+                                onChange={(e) => {
+                                    setCourses(e.target.value)
+                                }}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="max_group_size">
+                            <Form.Label>Kontenjan</Form.Label>
+                            <Form.Control
+                                onChange={(e) => {
+                                    setMax_group_size(e.target.value)
+                                }}
+                                type="number"
+                                value={max_group_size || ''}
                             />
                         </Form.Group>
                     </Form>
--- src/pages/SemesterCoursePage/index.js
+++ src/pages/SemesterCoursePage/index.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import Table from '../../components/general/Table';
-import { Button } from 'react-bootstrap';
-import { TrashFill } from 'react-bootstrap-icons';
+import {Button} from 'react-bootstrap';
+import {TrashFill} from 'react-bootstrap-icons';
 import ConfirmModal from '../../components/general/Modal/ConfirmModal';
 import {notificationActions} from "../../store/notification/notification-slice";
 import {useDispatch} from "react-redux";
@@ -10,51 +10,58 @@ import SemesterCourseModal from "./SemesterCourseModal";
 
 export const SemesterCoursePage = () => {
 
-  const { data, isLoading } = useGetAllSemesterCoursesQuery();
+    const {data, isLoading} = useGetAllSemesterCoursesQuery();
 
-  const handleDelete = async (id) => {
-      alert(id);
-  };
+    const handleDelete = async (id) => {
+        alert(id);
+    };
 
-  console.log(data);
-  return (
-    <>
-      <div>
-        {data?.results && !isLoading ? (
-          <Table
-            tableTitle="Dönem Dersi Listesi"
-            searchable={true}
-            addNewEntry={<SemesterCourseModal />}
-            head={[
-              { name: 'ID', sortable: 'numeric', width: 1 },
-              { name: 'Dönem', sortable: 'alpha' },
-              { name: 'Yıl', sortable: 'numeric' },
-              { name: 'Eylem', width: 1 },
-            ]}
-            body={data.results.map((semester_course) => [
-                semester_course.id,
-              ['Güz', 'Bahar', 'Yaz'][semester_course.term],
-              `${semester_course.year} - ${semester_course.year + 1}`,
-              <>
-                <SemesterCourseModal isEdit={true} data={semester_course} />
-                <ConfirmModal
-                  title="Dönem Dersi Silme"
-                  body="Bu dönem dersini silmek istediğinizden emin misiniz?"
-                  onConfirm={() => handleDelete(semester_course.id)}
-                  btn={
-                    <>
-                      <TrashFill size={15} />
-                      <span className="d-none d-md-block">Sil</span>
-                    </>
-                  }
-                />
-              </>,
-            ])}
-          />
-        ) : (
-          'Loading...'
-        )}
-      </div>
-    </>
-  );
+    console.log(data);
+    return (
+        <>
+            <div>
+                {data?.results && !isLoading ? (
+                    <Table
+                        tableTitle="Dönem Dersi Listesi"
+                        searchable={true}
+                        addNewEntry={<SemesterCourseModal/>}
+                        head={[
+                            {name: 'ID', sortable: 'numeric', width: 1},
+                            {name: 'Dönem', sortable: 'numeric'},
+                            {name: 'Ders', sortable: 'alpha'},
+                            {name: 'Kontenjan', sortable: 'alpha'},
+                            {name: 'Eylem', width: 1},
+                        ]}
+                        body={data.results.map((semester_course) => [
+                            semester_course.id,
+                            `${semester_course.semester.year} 
+                            - ${semester_course.semester.year + 1} /
+                             ${semester_course.term}
+                            `,
+                            semester_course.courses,
+                            semester_course.max_group_size,
+
+
+                            <>
+                                <SemesterCourseModal isEdit={true} data={semester_course}/>
+                                <ConfirmModal
+                                    title="Dönem Dersi Silme"
+                                    body="Bu dönem dersini silmek istediğinizden emin misiniz?"
+                                    onConfirm={() => handleDelete(semester_course.id)}
+                                    btn={
+                                        <>
+                                            <TrashFill size={15}/>
+                                            <span className="d-none d-md-block">Sil</span>
+                                        </>
+                                    }
+                                />
+                            </>,
+                        ])}
+                    />
+                ) : (
+                    'Loading...'
+                )}
+            </div>
+        </>
+    );
 };
