b519926a86eda365dcdfb80fcadb7b2a2b2cfb94
yasinsametkaraca <80101882+yasinsametkaraca@users.noreply.github.com>
2023-06-11 15:46:52 +0300

    Sayfalarda veri eksikliğinde oluşan hatalar giderildi.

--- src/pages/GroupDetailPage/GroupMembersSection.js
+++ src/pages/GroupDetailPage/GroupMembersSection.js
@@ -4,7 +4,7 @@ export const GroupMembersSection = ({ group }) => {
   return (
     <div className="group_members" style={{ background: 'rgba(0,255,0,.2)' }}>
       <h5>Grup Üyeleri</h5>
-      <pre>{JSON.stringify(group.group_members, null, 2)}</pre>
+      <pre>{JSON.stringify(group?.group_members, null, 2)}</pre>
     </div>
   );
 };
