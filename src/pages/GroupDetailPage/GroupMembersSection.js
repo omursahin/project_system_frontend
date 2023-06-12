c24b62c52252fb3a22dd8e7b5526b739b251403b
Süleyman Özgür Özarpacı <oozarpac@gmail.com>
2023-06-12 21:01:22 +0300

    grup üyeleri grup detayına eklendi

--- src/pages/GroupDetailPage/GroupMembersSection.js
+++ src/pages/GroupDetailPage/GroupMembersSection.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Badge, Card, ListGroup } from 'react-bootstrap';
+import { CheckCircleFill, XCircleFill } from 'react-bootstrap-icons';
 
 export const GroupMembersSection = ({ group }) => {
   return (
     <Card className='mt-3'>
       <Card.Header>Grup Üyeleri</Card.Header>
       <Card.Body>
-        {JSON.stringify(group?.group_members, null, 2)}
+        <ListGroup as="ol" numbered className=' list-group-flush'>
+          {group?.group_members?.map((member) => (
+            <ListGroup.Item
+              as="li"
+              className="d-flex justify-content-between align-items-start"
+            >
+              <div className="ms-2 me-auto">
+                <div className="fw-bold">{member.member.first_name} {member.member.last_name} {member?.is_supervisor && <Badge pill className="group_info_badge" bg="primary">
+                  Supervisor
+                </Badge>}
+                </div>
+                <small className='text-muted'>{member.member.email}</small>
+              </div>
+              {member?.is_accepted ? <CheckCircleFill className="text-success" /> : <XCircleFill className="text-danger" />}
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
       </Card.Body>
     </Card>
   );
