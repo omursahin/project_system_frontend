0366beb49b7fc1c3278d08b8a8bfab6ada5c8f37
zahidsgn <zahide.dusgun@gmail.com>
2023-06-13 21:29:29 +0300

    Spinner tasarımı değiştirildi

--- src/components/general/Loading/index.js
+++ src/components/general/Loading/index.js
@@ -5,13 +5,7 @@ export const Loading = () => {
     const isLoading = useSelector((state) => state.notification.isLoading);
 
     return isLoading &&
-        <Row className="justify-content-md-center">
-            <Spinner animation="grow" size='sm' className='mr-2' variant="primary"/>
-            <Spinner animation="grow" size='sm' className='mr-2' variant="secondary"/>
-            <Spinner animation="grow" size='sm' className='mr-2' variant="success"/>
-            <Spinner animation="grow" size='sm' className='mr-2' variant="danger"/>
-            <Spinner animation="grow" size='sm' className='mr-2' variant="warning"/>
-            <Spinner animation="grow" size='sm' className='mr-2' variant="info"/>
-            <Spinner animation="grow" size='sm' className='mr-2' variant="dark"/>
+        <Row className="justify-content-md-center mt-5">
+            <Spinner animation="border" variant="primary" /> <br/>
         </Row>
 }
